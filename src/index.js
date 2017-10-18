26528fca9118b6822552dc812469861420a06c49
David Johnston <davidj@datacom.co.nz>
2017-10-18 13:45:39 +1300

    Basic functionality working

--- src/index.js
+++ src/index.js
@@ -1,9 +1,76 @@
 import React from 'react';
-class Fancy extends React.Component {
+import "./style.scss";
+
+class SimpleCarousel extends React.Component {
+
+
+
+  constructor() {
+    super();
+    this.state = {i:0};
+
+  }
+
+  componentWillMount() {
+
+
+
+
+    this.setState({ i: 0, currentSelected: this.props.sourceObjects[0], max: this.props.sourceObjects.length -1});
+
+
+  }
+
+
+  update(inc){
+    let i = this.state.i + (inc? 1 : -1);
+
+    let selected = this.props.sourceObjects[i];
+
+    console.log(i);
+
+    this.setState({i:i, currentSelected: selected, leftDiabled : i === 0});
+
+  }
+
+  leftClick() {
+
+    console.log("lasdaeft");
+    this.update(false);
+
+    this.props.onChange(this.state.currentSelected);
+  }
+
+  rightClick() {
+
+    console.log("righ aadft");
+    console.log(this);
+    console.log(this.state);
+
+    this.update(true);
+
+
+
+    this.props.onChange(this.state.currentSelected);
+  }
+
   render() {
+
     return (
-      <div>This is so Fancy!</div>
+      <div className ="simple-carousel">
+
+        <button className  ="left" onClick ={() => {this.leftClick();}} disabled={this.state.i === 0} />
+
+          <div className ="simple-carousel-body">{this.props.sourceObjects.map((v,i) => {
+            let id = this.props.name + i;
+            let label = this.props.labelFn(v);
+            return <div > <input type = "radio" checked = {this.state.i === i} name = {this.props.name} key = {id} value = {label} /><label for = {id} > {this.state.i}{label}</label></div>;
+          })}</div>
+
+        <button className =  "right" onClick = {() => {this.rightClick();}} disabled ={this.state.i === this.state.max}/>
+
+      </div>
     );
   }
 }
-export default Fancy;
+export default SimpleCarousel;
